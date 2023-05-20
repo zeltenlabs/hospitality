d9083563b4838750e6bc9921236dc032f8c1c1f8
parrasajad <parrasajad@gmail.com>
2023-05-20 12:37:52 +0530

    add hotel doctypes and reports

---
+++ hospitality/hotel/doctype/hotel_room_pricing/hotel_room_pricing.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, ZeltenLabs and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class HotelRoomPricing(Document):
+	pass
---
+++ hospitality/hotel/doctype/hotel_room_type/hotel_room_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, ZeltenLabs and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Hotel Room Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
