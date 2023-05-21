78f08c36c247fdd41cfbadbf782c9fba35b4948a
parrasajad <parrasajad@gmail.com>
2023-05-21 10:36:46 +0530

    add occupancy chart on room form view

--- hospitality/hotel/doctype/hotel_room/hotel_room.js
+++ hospitality/hotel/doctype/hotel_room/hotel_room.js
@@ -1,8 +1,33 @@
 // Copyright (c) 2023, ZeltenLabs and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Hotel Room", {
-// 	refresh(frm) {
+frappe.ui.form.on("Hotel Room", {
+	refresh(frm) {
+        // Fetch the occupancy data from the server.
+        frappe.call({
+            method: 'hospitality.hotel.doctype.hotel_room.hotel_room.get_occupancy_data',
+            args: {
+                room_name: frm.doc.name
+            },
+            callback: function(response) {
+                var dataPoints = response.message;
 
-// 	},
-// });
+                var occupancyData = {
+                    dataPoints: dataPoints,
+                    start: new Date(), // Today
+                    end: new Date(new Date().setFullYear(new Date().getFullYear() + 1)), // Today + 1 year
+                };
+                // console.log("occupancy data::::",occupancyData, frm.doc.name);
+
+                // Create the chart.
+                var chart = new frappe.Chart('#occupancy_chart-container', {
+                    title: 'Room Occupancy',
+                    data: occupancyData,
+                    type: 'heatmap',
+                });
+                
+                chart.render();
+            }
+        });
+	},
+});
--- hospitality/hotel/doctype/hotel_room/hotel_room.py
+++ hospitality/hotel/doctype/hotel_room/hotel_room.py
@@ -1,9 +1,44 @@
 # Copyright (c) 2023, ZeltenLabs and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate, add_days
+from dateutil.rrule import rrule, DAILY
+import datetime
 
 
 class HotelRoom(Document):
 	pass
+
+@frappe.whitelist()
+def get_occupancy_data(room_name):
+    # Fetch all reservations.
+    reservations = frappe.get_all('Hotel Room Reservation', fields=['name', 'from_date', 'to_date'])
+
+    # Filter out reservations that do not include the room.
+    reservations_for_room = []
+    for reservation in reservations:
+        rooms = frappe.get_all('Hotel Room Reservation Detail', filters={'parent': reservation.name, 'hote_room': room_name}, fields=['hote_room'])
+        if rooms:
+            reservations_for_room.append(reservation)
+
+    # Sort the reservations by from_date.
+    reservations_for_room.sort(key=lambda r: r.from_date)
+
+    # Initialize the data points dictionary.
+    data_points = {}
+
+    # Iterate over the reservations and count the number of reservations for each date.
+    for reservation in reservations_for_room:
+        start_date = getdate(reservation['from_date'])
+        end_date = getdate(reservation['to_date'])
+
+        for dt in rrule(DAILY, dtstart=start_date, until=end_date):
+            timestamp = int(dt.timestamp())
+            if timestamp in data_points:
+                data_points[timestamp] += 1
+            else:
+                data_points[timestamp] = 1
+
+    return data_points
