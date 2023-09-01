581c4137b15077b2a80303666c5df34606bfbc8f
parrasajad <parrasajad@gmail.com>
2023-09-01 18:39:49 +0530

    server script to update reservation status on checkin/checkout

--- hospitality/hotel/doctype/checkout/checkout.py
+++ hospitality/hotel/doctype/checkout/checkout.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2023, ZeltenLabs and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Checkout(Document):
-	pass
+	def after_insert(self):
+		# Check if the linked field is set in the current document
+		if self.reservation:
+			reservation = frappe.get_doc("Hotel Room Reservation", self.reservation)
+			reservation.status = "Completed"
+			reservation.save()
